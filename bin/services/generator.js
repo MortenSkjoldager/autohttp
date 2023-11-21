1e754f642ee2fe94e833a72928599e80b3a20f61
VERTICA\msj <msj@vertica.dk>
2023-11-21 12:51:58 +0100

    Support for inline or variable mode in path and query.

--- bin/services/generator.js
+++ bin/services/generator.js
@@ -82,20 +82,32 @@ async function generateForPath(key, apiPath, spec, workspaceSettings, config) {
   }
 }
 
-function replacePathVariables(segment, operation) {
+function generateVariableNameFromOperation(operation, parameter) {
+  return operation.operationId+'_'+parameter.name;
+}
+
+function getValueOrVariable(operation, parameter, config) {
+  if (config.pathAndQueryMode != null && config.pathAndQueryMode == 'var') {
+    return '{{'+generateVariableNameFromOperation(operation, parameter)+'}}';
+  }
+
+  return valueFactory.getValueFromType(parameter.schema.type);
+}
+
+function replacePathVariables(segment, operation, config) {
   var replacedPath = segment.fullPath;
   if (!operation.parameters) { 
     return replacedPath; 
   }
 
   for (let param of operation.parameters.filter((param) => param.in == 'path')) {
-    replacedPath = replacedPath.replace(`{${param.name}}`, valueFactory.getValueFromType(param.schema.type))
+    replacedPath = replacedPath.replace(`{${param.name}}`, getValueOrVariable(operation, param, config))
   }
 
   const query = operation
                   .parameters
                   .filter((param) => param.in == 'query')
-                  .map((x) => x.name+'='+valueFactory.getValueFromType(x.schema.type)).join('&');
+                  .map((x) => x.name+'='+getValueOrVariable(operation, x, config)).join('&');
 
   if (query != null && query != '') {
     replacedPath = replacedPath + '?' + query;
@@ -106,19 +118,25 @@ function replacePathVariables(segment, operation) {
 
 async function generateForVerb(verb, operation, segment, spec, apiOutputPath, workspaceSettings, config) {
   const lines = [];
-  lines.push(`# @name ${verb}_${segment.area}_${segment.method}`)
-  lines.push(`${verb.toUpperCase()} {{${config.baseUrlEnvKey}}}${replacePathVariables(segment,operation)}`)
+  if (config.pathAndQueryMode && config.pathAndQueryMode == 'var' && operation.parameters && Array.isArray(operation.parameters)) {
+    for (let parameter of operation.parameters) {
+      lines.push('@'+generateVariableNameFromOperation(operation, parameter)+'='+valueFactory.getValueFromType(parameter.schema.type))
+    }
+  }
 
+  lines.push(`# @name ${verb}_${segment.area}_${operation.operationId}`)
+  lines.push(`${verb.toUpperCase()} {{${config.baseUrlEnvKey}}}${replacePathVariables(segment,operation, config)}`)
+  lines.push('\n');
   const schemaRef = schemaIntepreter.verbAcceptsJsonInput(operation);
   if (schemaRef != null) {
-    lines.push("Content-Type: application/json");
-    lines.push("");
+    lines.push('Content-Type: application/json');
+    lines.push('\n');
     var postJson = schemaIntepreter.generateObjectFromRef(spec.components, schemaRef);
     const formattedJson = JSON.stringify(postJson, null, 2);
     lines.push(formattedJson);
   }
 
-  await fileWriter.writeFileWithContents(path.join(apiOutputPath, verb+"_"+segment.method+"_"+operation.operationId+".http"), lines.join('\n'));
+  await fileWriter.writeFileWithContents(path.join(apiOutputPath, verb+"_"+operation.operationId+".http"), lines.join('\n'));
 }
 
 module.exports = { generateFolder, initializeWorkspace, initializeEnvironments, generateForPath, initializeYacConfig }
